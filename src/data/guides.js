dc7204230c0661996cfc375e2ffd3d15c906691b
Nick Beattie <nick@nickbytes.com>
2018-01-15 13:24:51 -0500

    initial commit

---
+++ src/data/guides.js
@@ -0,0 +1,32 @@
+export const guides = [
+  {
+    id: 1,
+    name: "Rough, Intense, Distortion",
+    knobs: [
+      {
+        id: 1,
+        valueTime: "10:30",
+        valuePercentage: 30,
+        modulate: false
+      },
+      {
+        id: 2,
+        valueTime: "12:00",
+        valuePercentage: 50,
+        modulate: true
+      },
+      {
+        id: 3,
+        valueTime: "12:00",
+        valuePercentage: 50,
+        modulate: false
+      },
+      {
+        id: 4,
+        valueTime: "8:00",
+        valuePercentage: 15,
+        modulate: false
+      }
+    ]
+  }
+];
